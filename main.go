7d914ebbfc7fc970cd255b496e631dbc024d0b9e
Ihor Kichuk <ikichuk@tucowsinc.com>
2024-02-16 10:58:54 -0600

    comments are added

--- main.go
+++ main.go
@@ -13,8 +13,7 @@ import (
 const timeMarkerFName = "timemarker"
 const timeMarkerFPath = "./" + timeMarkerFName
 
-var helpMessage = "command name is missed,\n" +
-	"options: start, stop"
+var helpMessage = "tag is missed"
 var pathError = &fs.PathError{}
 
 func main() {
@@ -24,10 +23,10 @@ func main() {
 		}
 	}()
 
-	op := getOperator()
+	tag, comment := getParams()
 
 	now := time.Now()
-	log, err := getLogFile(&now)
+	log, err := getLogFile(now)
 	defer log.Close()
 
 	mBytes, err := os.ReadFile(timeMarkerFPath)
@@ -35,14 +34,14 @@ func main() {
 		panic(err)
 	}
 
-	var lastOperation string
+	var lastTag string
 	var marker int
 	if err == nil {
-		lastOperation, marker = getMarkers(mBytes)
+		lastTag, marker = getMarkers(mBytes)
 	}
 
-	if op == lastOperation {
-		panic("New operator has to be different from the last one.")
+	if tag == lastTag {
+		panic("New tag has to be different from the last one.")
 	}
 
 	mFile, err := os.OpenFile(timeMarkerFPath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
@@ -51,25 +50,26 @@ func main() {
 	}
 	defer mFile.Close()
 
-	hours, mins := getTimeDIfference(marker, &now)
-	if _, err = log.WriteString(fmt.Sprintf("%s,%s,%d,%d\n", now.Format(time.RFC3339), op, hours, mins)); err != nil {
+	hours, mins := getTimeDifference(marker, now)
+	if _, err = log.WriteString(fmt.Sprintf("%s,%s,%d,%d,%s\n", now.Format(time.RFC3339), tag, hours, mins, comment)); err != nil {
 		panic(err)
 	}
 
-	if _, err = mFile.WriteString(fmt.Sprintf("%s\n%d\n", op, now.Unix())); err != nil {
+	if _, err = mFile.WriteString(fmt.Sprintf("%s\n%d\n", tag, now.Unix())); err != nil {
 		panic(err)
 	}
 
 	fmt.Println("OK")
 }
 
-func getTimeDIfference(marker int, now *time.Time) (int, int) {
+func getTimeDifference(marker int, now time.Time) (int, int) { //bench it
 	var hours, mins int
 	if marker != 0 {
 		timeMarker := time.Unix(int64(marker), 0)
 		diff := now.Sub(timeMarker)
 		hours, mins = int(diff.Hours()), int(diff.Minutes())%60
 	}
+
 	return hours, mins
 }
 
@@ -83,11 +83,12 @@ func getMarkers(mBytes []byte) (string, int) {
 	if err != nil {
 		panic(err)
 	}
+
 	return lastOperation, marker
 }
 
 // the caller of the function IS RESPONSIBLE for closing the returned file
-func getLogFile(now *time.Time) (*os.File, error) {
+func getLogFile(now time.Time) (*os.File, error) {
 	fname := fmt.Sprintf("%d-%d-%d.csv", now.Year(), now.Month(), now.Day())
 	log, err := os.OpenFile("./logs/"+fname, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
@@ -97,20 +98,18 @@ func getLogFile(now *time.Time) (*os.File, error) {
 	return log, err
 }
 
-func getOperator() string {
+func getParams() (string, string) {
 	args := os.Args
 	if len(args) < 2 {
 		fmt.Println(helpMessage)
 		os.Exit(1)
 	}
 
-	switch args[1] {
-	case "start":
-	case "stop":
-	default:
-		fmt.Println(helpMessage)
-		os.Exit(1)
+	tag := args[1]
+	var comment string
+	if len(args) > 2 {
+		comment = args[2]
 	}
 
-	return args[1]
+	return tag, comment
 }
